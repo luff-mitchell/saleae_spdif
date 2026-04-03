a17c9efc476378f0e355ea9f1269762e57b50dbe
luff-mitchell <mitchell.luff@gmail.com>
2026-04-03 20:30:11 +0900

    Update spdifAnalyzer.h

--- source/spdifAnalyzer.h
+++ source/spdifAnalyzer.h
@@ -36,19 +36,47 @@ extern "C" {
 #include "wavhdr.h"
 };
 
+/* ------------------------------------------------------------------ */
+/* IEC 61937 버스트 구조 파싱용 상태머신                                 */
+/*  Pa(0xF872) -> Pb(0x4E1F) -> Pc(data-type) -> Pd(length)           */
+/* ------------------------------------------------------------------ */
+enum Iec61937State {
+    IEC61937_IDLE,      /* Pa 대기 중          */
+    IEC61937_GOT_PA,    /* Pa 감지, Pb 대기    */
+    IEC61937_GOT_PB,    /* Pb 감지, Pc 대기    */
+    IEC61937_GOT_PC,    /* Pc 감지, Pd 대기    */
+};
+
+/* IEC 61937 Pc 워드 하위 5비트 data-type 코드 */
+#define IEC61937_AC3        0x01
+#define IEC61937_MPEG1_L1   0x02
+#define IEC61937_MPEG1_L23  0x03
+#define IEC61937_MPEG2_LSF  0x04
+#define IEC61937_DTS_I      0x06
+#define IEC61937_DTS_II     0x07
+#define IEC61937_DTS_III    0x08
+#define IEC61937_DTS_IV     0x0B
+#define IEC61937_MPEG2_AAC  0x0C
+#define IEC61937_EAC3       0x15
+#define IEC61937_TRUEHD     0x16
+
+/* Frame mType 구분 상수 */
+#define FRAME_TYPE_CHANNEL_STATUS   0xFE   /* Channel Status 블록 프레임 */
+#define FRAME_TYPE_IEC61937         0xFD   /* IEC 61937 포맷 감지 프레임  */
+
 class spdifAnalyzerSettings;
 class ANALYZER_EXPORT spdifAnalyzer : public Analyzer2
 {
 public:
-	spdifAnalyzer();
-	virtual ~spdifAnalyzer();
-	virtual void WorkerThread();
+    spdifAnalyzer();
+    virtual ~spdifAnalyzer();
+    virtual void WorkerThread();
 
-	virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
-	virtual U32 GetMinimumSampleRateHz();
+    virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
+    virtual U32 GetMinimumSampleRateHz();
 
-	virtual const char* GetAnalyzerName() const;
-	virtual bool NeedsRerun();
+    virtual const char* GetAnalyzerName() const;
+    virtual bool NeedsRerun();
     virtual void SetupResults();
 
     /* callbacks from the "C" bitstream analyzer library */
@@ -56,21 +84,21 @@ public:
     void status_callback( uint64_t t, uint64_t tend, struct SpdifChannelStatus *status );
 
 protected: //vars
-	std::auto_ptr< spdifAnalyzerSettings > mSettings;
-	std::auto_ptr< spdifAnalyzerResults > mResults;
-	AnalyzerChannelData* mSerial;
+    std::auto_ptr< spdifAnalyzerSettings > mSettings;
+    std::auto_ptr< spdifAnalyzerResults >  mResults;
+    AnalyzerChannelData*                   mSerial;
 
-	spdifSimulationDataGenerator mSimulationDataGenerator;
-	bool mSimulationInitilized;
+    spdifSimulationDataGenerator mSimulationDataGenerator;
+    bool                         mSimulationInitilized;
 
-	//Serial analysis vars:
-	U32 mSampleRateHz;
-	U32 mStartOfStopBitOffset;
-	U32 mEndOfStopBitOffset;
+    /* Serial analysis vars */
+    U32 mSampleRateHz;
+    U32 mStartOfStopBitOffset;
+    U32 mEndOfStopBitOffset;
 
-	U16 m_PrevPCM;
-	U16 m_Pad0;
-	U32 m_AC3_Detected;	/* number of times AC3 frame headers have been noticed */
+    U16 m_PrevPCM;
+    U16 m_Pad0;
+    U32 m_AC3_Detected;
 
     /* "C" bitstream parser library */
     struct SpdifBitstreamAnalyzer *mSba;
@@ -79,6 +107,15 @@ protected: //vars
     uint64_t                       mPrevSampleEnd;
     uint64_t                       mPrevStatus;
     uint64_t                       mPrevStatusEnd;
+
+    /* ---- Channel Status (IEC 60958) -------------------------------- */
+    struct SpdifChannelStatus      mLastChannelStatus;
+    bool                           mHasChannelStatus;
+
+    /* ---- IEC 61937 Non-audio 포맷 감지 상태머신 -------------------- */
+    enum Iec61937State  mIecState;      /* 현재 상태                    */
+    uint8_t             mIecDataType;   /* Pc 워드에서 읽은 data-type   */
+    uint64_t            mIecBurstStart; /* Pa 감지 시각                 */
 };
 
 extern "C" ANALYZER_EXPORT const char* __cdecl GetAnalyzerName();
